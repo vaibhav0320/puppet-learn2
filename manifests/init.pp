7a25c8eb731568512f860fa73351bdb638aff78b
Vaibhav Chauhan <vaibhav.chauhan@newfold.com>
2023-04-25 12:14:51 +0530

    added default node

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,11 @@
+node default{
 
 file { 'helo.txt':
   path => '/etc/helo.txt',
   ensure => file,
   content => "my os is $operatingsystem"
   
-}
+} } 
 
 node 'websrv'{
 
--- modules/test/manifests/init.pp
+++ modules/test/manifests/init.pp
@@ -3,7 +3,7 @@ class test{
   file { 'test.txt':
     path => '/etc/test.txt',
     ensure => file,
-    content => "This file if from test module"
+    content => "This file if from test module..after update"
     
   }
 
